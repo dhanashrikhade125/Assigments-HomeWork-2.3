3b31f2929d7865ed219fe372a0331e585d20a417
DhanashriKhade125 <coder.dhanashrikhade12@gmail.com>
2022-12-30 18:22:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25, b = 30;
+            Console.WriteLine("Before swap a= " + a + " b= " + b); ;
+            a = a * b;       
+            b = a / b;     
+            a = a / b;    
+            Console.WriteLine("After swap a= " + a + " b= " + b);
+        }
+    }
+}
